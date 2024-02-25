3d493b9cb8aa38f299911ce9cc1ebba2dbb8f498
molmezses <mustafaolmezses@icloud.com>
2024-02-25 16:48:17 +0300

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Mustafa Ölmezses on 25.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
