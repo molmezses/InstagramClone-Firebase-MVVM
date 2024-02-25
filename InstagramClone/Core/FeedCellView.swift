e826fde38eaf76677300c2d3c1058a5cbfba6dc4
molmezses <mustafaolmezses@icloud.com>
2024-02-26 01:49:19 +0300

    Added FeedView and FeedCell

---
+++ InstagramClone/Core/FeedCellView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedCellView.swift
+//  InstagramClone
+//
+//  Created by Mustafa Ölmezses on 26.02.2024.
+//
+
+import SwiftUI
+
+struct FeedCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedCellView()
+}
